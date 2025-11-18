25495cb5ed93f56c4d29e9b1003cc145e28f2763
sankar58 <ayudhamsankar@gmail.com>
2025-11-18 19:10:44 +0530

    added session and flash functionality

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const Review = require('./models/review');
+const session=require('express-session')
+const flash = require("connect-flash")
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -25,6 +27,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig ={
+    secret :'thisshouldbeabettersecret',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        httpOnly:true,
+        expires:Date.now()+1000 *60*60*24*7,
+        maxAge :1000 *60*60*24*7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash())
+app.use((req,res,next)=>{
+    res.locals.success=req.flash('success');
+    res.locals.error=req.flash('error');
+    next();
+})
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
